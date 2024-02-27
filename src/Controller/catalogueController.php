6491033bd62bc53f646acd73c7ef8fd941156779
LoicLeonetti <2066177@cstj.qc.ca>
2024-02-27 15:55:27 -0500

    Filters done

--- src/Controller/catalogueController.php
+++ src/Controller/catalogueController.php
@@ -2,63 +2,114 @@
 
 namespace App\Controller;
 
+# Nom: Loic Leonetti #
+# Date: 2024-02-24   #
+
 use App\Entity\Categorie;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 use Doctrine\Persistence\ManagerRegistry;
 use App\Entity\Produit;
 
-use function PHPSTORM_META\map;
-
 class catalogueController extends AbstractController
 {
+    #--------------------------------------------------------------------------------#
+    #--------------------------------------------------------------------------------#
+    #--------------------------------------------------------------------------------#
     #[Route('/', name: 'catalogue')]
     public function accueil(ManagerRegistry $doctrine): Response
     {
-        $produits = $doctrine -> getManager() ->getRepository(Produit::class)->findAll();
+        $produits = $doctrine->getManager()->getRepository(Produit::class)->findAll();
 
-        $categories = $doctrine -> getManager()->getRepository(Categorie::class)->findAll();
+        $categories = $doctrine->getManager()->getRepository(Categorie::class)->findAll();
 
-        return $this->render('catalogue.html.twig',['produits' => $produits,'categories' => $categories]);
+        return $this->render('catalogue.html.twig', ['produits' => $produits, 'categories' => $categories]);
     }
 
 
-    #[Route('/{id}', name: 'filtre_categorie')]
-    public function filtre_categorie(ManagerRegistry $doctrine,$id): Response
-    {
-        
-        $produits = $doctrine -> getManager() ->getRepository(Produit::class)->findBy(array("idCategorie"=>$id));
-
-        $categories = $doctrine -> getManager()->getRepository(Categorie::class)->findAll();
-
-        return $this->render('catalogue.html.twig',['produits' => $produits,'categories' => $categories]);
-    }
-
-    #[Route('/{recherche}', name: 'filtre_recherche')]
-    public function filtre_recherche(ManagerRegistry $doctrine,$recherche): Response
+    #--------------------------------------------------------------------------------#
+    #--------------------------------------------------------------------------------#
+    #--------------------------------------------------------------------------------#
+    #[Route('/filtre', name: 'filtre')]
+    public function filtre(ManagerRegistry $doctrine, Request $request): Response
     {
 
-        //TODO
-        
-        $produits = $doctrine -> getManager() ->getRepository(Produit::class)->findBy(array('Nom'=> $recherche,"Description"=>$recherche));
+        $produits = $doctrine->getManager()->getRepository(Produit::class)->findAll();
+        $categories = $doctrine->getManager()->getRepository(Categorie::class)->findAll();
 
-        $categories = $doctrine -> getManager()->getRepository(Categorie::class)->findAll();
 
-        return $this->render('catalogue.html.twig',['produits' => $produits,'categories' => $categories]);
+        // Filtre Texte
+        $texteFiltre = $request->query->get('texteFiltre');
+        $categorieId = $request->query->get('categorieFiltre');
+
+        if (strlen($texteFiltre) != 0) {
+
+            $produits = $this->filtrerTexte($produits, $texteFiltre);
+        }
+
+        // Filtre Categorie
+        if ($categorieId != "Toutes") {
+            $produits = $this->filtrerCategorie($produits, $categorieId);
+        }
+
+        return $this->render('catalogue.html.twig', ['produits' => $produits, 'categories' => $categories]);
     }
 
+    #--------------------------------------------------------------------------------#
+    #--------------------------------------------------------------------------------#
+    #--------------------------------------------------------------------------------#
+    private function filtrerTexte($produits, $texteFiltre)
+    {
+        $tabTMP = [];
+
+        // Ajoute les produits dans le liste si le nom ou la description du produit contient la recherche
+        foreach ($produits as $p) {
+
+            if (strpos($p->getNom(), $texteFiltre) !== false) {
+                $tabTMP[] = $p;
+            }
+        }
+
+        return $tabTMP;
+    }
+
+    #--------------------------------------------------------------------------------#
+    #--------------------------------------------------------------------------------#
+    #--------------------------------------------------------------------------------#
+    private function filtrerCategorie($produits, $categorieId)
+    {
+
+        $tabTMP = [];
+        // Ajoute les produits dans le liste si le categorieId est le meme que le recherche
+        foreach ($produits as $p) {
+            if ($p->getIdCategorie() == $categorieId) {
+
+                $tabTMP[] = $p;
+            }
+        }
+
+        return $tabTMP;
+    }
+
+    #--------------------------------------------------------------------------------#
+    #--------------------------------------------------------------------------------#
+    #--------------------------------------------------------------------------------#
     #[Route('/produit_details/{id}', name: 'produit_details')]
-    public function produit_details(ManagerRegistry $doctrine,$id): Response
+    public function produit_details(ManagerRegistry $doctrine, $id): Response
     {
 
         $produit = $doctrine->getManager()->getRepository(Produit::class)->find($id);
-        $categorie = $doctrine -> getManager()->getRepository(Categorie::class)->find($produit->getIdCategorie());
+        $categorie = $doctrine->getManager()->getRepository(Categorie::class)->find($produit->getIdCategorie());
 
-        return $this->render("detailsProduit.html.twig",["produit"=>$produit,"categorie"=>$categorie]);
+        return $this->render("detailsProduit.html.twig", ["produit" => $produit, "categorie" => $categorie]);
     }
 
+    #--------------------------------------------------------------------------------#
+    #--------------------------------------------------------------------------------#
+    #--------------------------------------------------------------------------------#
     #[Route('/contact', name: 'contact')]
     public function contact(): Response
     {
@@ -66,4 +117,3 @@ class catalogueController extends AbstractController
         return $this->render("contact.html.twig");
     }
 }
-
